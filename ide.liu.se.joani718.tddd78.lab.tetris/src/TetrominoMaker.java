6c0529de6e331eff9631498b5ab81db1dfa03057
Jojoje <jojoje.nilsson@gmail.com>
2016-02-09 15:04:04 +0100

    Stuck on TetrominoMaker...

--- ide.liu.se.joani718.tddd78.lab.tetris/src/TetrominoMaker.java
+++ ide.liu.se.joani718.tddd78.lab.tetris/src/TetrominoMaker.java
@@ -1,3 +1,35 @@
 public class TetrominoMaker {
-    
+
+    public int getNumberOfTypes() {
+
+    }
+
+    public Poly getPoly(int n) {
+	Poly poly = createPoly(SquareType.values()[n]);
+	return poly;
+    }
+
+    public Poly createPoly(SquareType ST){
+	int witdh, height;
+
+	if(ST == SquareType.I){
+	    Poly poly = new Poly(4,4);
+	}
+	else if(ST == SquareType.S || ST == SquareType.T || ST == SquareType.Z || ST == SquareType.J || ST == SquareType.L){
+	    Poly poly = new Poly(3,3);
+	}
+	else if(ST == SquareType.O){
+	    Poly poly = new Poly(2,2);
+	}
+
+
+	switch(ST){
+	    case I:
+		Poly poly = new Poly(4,4);
+		poly.
+
+	}
+
+	return poly;
+    }
 }
